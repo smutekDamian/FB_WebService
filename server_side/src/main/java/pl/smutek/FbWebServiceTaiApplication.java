cee443db2f2ca5f41f7bc027237da030807bc5e8
smutekDamian <smutek.damian95@gmail.com>
2017-05-15 22:00:44 +0200

    Initial connection with Facebook

--- server_side/src/main/java/pl/smutek/FbWebServiceTaiApplication.java
+++ server_side/src/main/java/pl/smutek/FbWebServiceTaiApplication.java
@@ -1,18 +1,18 @@
 package pl.smutek;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class FbWebServiceTaiApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FbWebServiceTaiApplication.class);
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(FbWebServiceTaiApplication.class, args);
--- server_side/src/main/java/pl/smutek/LoginController.java
+++ server_side/src/main/java/pl/smutek/LoginController.java
@@ -1,19 +1,41 @@
 package pl.smutek;
 
 
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Post;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by damian on 04.03.17.
  */
-@RestController
+@Controller
+@RequestMapping("/api/login")
 public class LoginController {
 
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
 
-    @RequestMapping("/api")
-    public String showLoginPage(){
-        return "login";
+    public LoginController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        model.addAttribute("facebookProfile", facebook.userOperations().getUserProfile());
+        PagedList<Post> feed = facebook.feedOperations().getFeed();
+        model.addAttribute("feed", feed);
+        return "hello";
     }
 
 }

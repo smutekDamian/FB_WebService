33a8118cf33cb0b69a7f1bcb0671403b2d84b735
smutekDamian <smutek.damian95@gmail.com>
2017-05-15 19:37:21 +0200

    Added first - login side

--- client_side/src/app/app.module.ts
+++ client_side/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
--- client_side/src/app/hero.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by damian on 21.04.17.
- */
-export class Hero {
-  id: number;
-  name: string;
-}

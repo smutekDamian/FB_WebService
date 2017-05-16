9ea0c4314c5389d5ac554c67ea2701f9adce5f16
smutekDamian <smutek.damian95@gmail.com>
2017-05-16 14:03:12 +0200

    Fixed bug in app.component

--- client_side/src/app/start/start.component.ts
+++ client_side/src/app/start/start.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-start',
   templateUrl: './start.component.html',
   // styleUrls: ['./start.component.css'],
 })
